4f267f649c31e018c26418e324114dda52502777
infinixs5lite <kelvinchinedu97@gmail.com>
2022-06-10 07:01:40 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,10 +6,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_Infinix-X688B.mk
+    $(LOCAL_DIR)/omni_Infinix-X688B.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Infinix-X688B-user \
-    twrp_Infinix-X688B-userdebug \
-    twrp_Infinix-X688B-eng
-    twrp_X668B_eng
+    omni_Infinix-X688B-user \
+    omni_Infinix-X688B-userdebug \
+    omni_Infinix-X688B-eng
+    omni_X668B_eng
